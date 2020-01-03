714888f3fca1bf8bf61f514b7047dd20c307a237
John Andersen <john.s.andersen@intel.com>
2020-01-03 15:15:41 -0800

    ci: Move to GitHub Actions
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- .ci/run.sh
+++ .ci/run.sh
@@ -1,48 +1,134 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -ex
 
-echo "Building style check image..."
-docker build -t tpm2software/tpm2-tss-python-black \
-  -f .ci/Dockerfile.tpm2-tss-python-black .
-echo "Style check image built"
+if [ -d "${HOME}/.local/bin" ]; then
+  export PATH="${HOME}/.local/bin:${PATH}"
+fi
 
-echo "Building docker image..."
-docker build -t tpm2software/tpm2-tss-python \
-  -f .ci/Dockerfile.tpm2-tss-python .
-echo "Docker image built"
+SRC_ROOT=${SRC_ROOT:-"${PWD}"}
+PYTHON=${PYTHON:-"python3.7"}
 
-echo "Running build..."
-docker run --rm \
-  -u $(id -u):$(id -g) \
-  -v "${PWD}:/workspace/tpm2-pytss" \
-  --env-file .ci/docker.env \
-  tpm2software/tpm2-tss-python \
-  /bin/bash -c 'python3 setup.py sdist && python3 setup.py bdist'
-echo "Build success"
+TEMP_DIRS=()
 
-echo "Checking style..."
-docker run --rm \
-  -u $(id -u):$(id -g) \
-  -v "${PWD}:/workspace/tpm2-pytss" \
-  tpm2software/tpm2-tss-python-black \
-  --check .
-echo "Style check passed"
+function run_test() {
+  docker run --rm \
+    -u $(id -u):$(id -g) \
+    -v "${PWD}:/workspace/tpm2-pytss" \
+    --env-file .ci/docker.env \
+    tpm2software/tpm2-tss-python \
+    /bin/bash -c 'python3 setup.py sdist && python3 setup.py bdist'
 
-echo "Running tests..."
-docker run --rm \
-  -u $(id -u):$(id -g) \
-  -v "${PWD}:/workspace/tpm2-pytss" \
-  --env-file .ci/docker.env \
-  tpm2software/tpm2-tss-python \
-  /bin/bash -c '/workspace/tpm2-pytss/.ci/docker.run'
-echo "Tests passed"
+  docker run --rm \
+    -u $(id -u):$(id -g) \
+    -v "${PWD}:/workspace/tpm2-pytss" \
+    --env-file .ci/docker.env \
+    tpm2software/tpm2-tss-python \
+    /bin/bash -c '/workspace/tpm2-pytss/.ci/docker.run'
 
-echo "Creating docs..."
-rm -rf public
-docker run --rm \
-  -u $(id -u):$(id -g) \
-  -v "${PWD}:/workspace/tpm2-pytss" \
-  --env-file .ci/docker.env \
-  tpm2software/tpm2-tss-python \
-  /bin/bash -c "./scripts/docs.sh && mv pages public"
-echo "Documentation generated successfully"
+  if [ "x${GITHUB_ACTIONS}" == "xtrue" ] && [ "x${GITHUB_REF}" == "xrefs/heads/master" ]; then
+    dffml service dev release .
+  fi
+}
+
+function run_whitespace() {
+  export whitespace=$(mktemp -u)
+  function rmtempfile () {
+    rm -f "$whitespace"
+  }
+  trap rmtempfile EXIT
+  find . -type f -name '*.py' -exec grep -EHn " +$" {} \; 2>&1 > "$whitespace"
+  lines=$(wc -l < "$whitespace")
+  if [ "$lines" -ne 0 ]; then
+    echo "Trailing whitespace found" >&2
+    cat "${whitespace}" >&2
+    exit 1
+  fi
+}
+
+function run_style() {
+  black --check "${SRC_ROOT}"
+}
+
+function run_docs() {
+  if [ "x${GITHUB_ACTIONS}" == "xtrue" ] && [ "x${GITHUB_REF}" != "xrefs/heads/master" ]; then
+    return
+  fi
+
+  mkdir -p ~/.ssh
+  chmod 700 ~/.ssh
+  "${PYTHON}" -c "import pathlib, base64, os; keyfile = pathlib.Path('~/.ssh/github_tpm2_pytss').expanduser(); keyfile.write_bytes(b''); keyfile.chmod(0o600); keyfile.write_bytes(base64.b32decode(os.environ['GITHUB_PAGES_KEY']))"
+  ssh-keygen -y -f ~/.ssh/github_tpm2_pytss > ~/.ssh/github_tpm2_pytss.pub
+  export GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentityFile=~/.ssh/github_tpm2_pytss'
+
+  cd "${SRC_ROOT}"
+
+  # Make master docs
+  master_docs="$(mktemp -d)"
+  TEMP_DIRS+=("${master_docs}")
+  rm -rf pages
+  docker run --rm \
+    -u $(id -u):$(id -g) \
+    -v "${PWD}:/workspace/tpm2-pytss" \
+    --env-file .ci/docker.env \
+    tpm2software/tpm2-tss-python \
+    /bin/bash -c ./scripts/docs.sh
+  mv pages "${master_docs}/html"
+
+  # Make last release docs
+  release_docs="$(mktemp -d)"
+  TEMP_DIRS+=("${release_docs}")
+  rm -rf pages
+  git clean -fdx
+  git checkout $(git describe --abbrev=0 --tags --match '*.*.*')
+  git clean -fdx
+  git reset --hard HEAD
+  docker run --rm \
+    -u $(id -u):$(id -g) \
+    -v "${PWD}:/workspace/tpm2-pytss" \
+    --env-file .ci/docker.env \
+    tpm2software/tpm2-tss-python \
+    /bin/bash -c ./scripts/docs.sh
+  mv pages "${release_docs}/html"
+
+  git clone git@github.com:tpm2-software/tpm2-pytss -b gh-pages \
+    "${release_docs}/old-gh-pages-branch"
+
+  mv "${release_docs}/old-gh-pages-branch/.git" "${release_docs}/html/"
+  mv "${master_docs}/html" "${release_docs}/html/master"
+
+  cd "${release_docs}/html"
+
+  git config user.name 'John Andersen'
+  git config user.email 'johnandersenpdx@gmail.com'
+
+  git add -A
+  git commit -sam "docs: $(date)"
+  git push
+
+  cd -
+
+  git reset --hard HEAD
+  git checkout master
+}
+
+function cleanup_temp_dirs() {
+  if [ "x${NO_RM_TEMP}" != "x" ]; then
+    return
+  fi
+  for temp_dir in ${TEMP_DIRS[@]}; do
+    rm -rf "${temp_dir}"
+  done
+}
+
+# Clean up temporary directories on exit
+trap cleanup_temp_dirs EXIT
+
+if [ "x${TEST}" != "x" ]; then
+  run_test
+elif [ "x${WHITESPACE}" != "x" ]; then
+  run_whitespace
+elif [ "x${STYLE}" != "x" ]; then
+  run_style
+elif [ "x${DOCS}" != "x" ]; then
+  run_docs
+fi
