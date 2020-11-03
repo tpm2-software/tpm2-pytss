8008c11fee036ec28d1551ed8426487c00adba01
John Andersen <johnandersenpdx@gmail.com>
2020-11-03 00:40:37 -0800

    tests: esys: get capability: Fixed to not use context management
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- tests/test_esys_get_capability.py
+++ tests/test_esys_get_capability.py
@@ -8,16 +8,6 @@ class TestGetCapability(BaseTestESYS):
         prop = TPM2_PT_LOCKOUT_COUNTER
         propertyCount = 1
 
-        with TPMI_YES_NO_PTR(
-            False
-        ) as moreData_ptr, TPMS_CAPABILITY_DATA_PTR_PTR() as capabilityData_ptr_ptr:
-            r = self.esys_ctx.GetCapability(
-                ESYS_TR_NONE,
-                ESYS_TR_NONE,
-                ESYS_TR_NONE,
-                capability,
-                prop,
-                propertyCount,
-                moreData_ptr,
-                capabilityData_ptr_ptr,
-            )
+        self.esys_ctx.GetCapability(
+            ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE, capability, prop, propertyCount,
+        )
