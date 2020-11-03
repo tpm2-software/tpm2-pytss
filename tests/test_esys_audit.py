14b8f4da4a7cd86bc467c6a138d0831ba1e4c5dc
John Andersen <johnandersenpdx@gmail.com>
2020-11-03 00:08:51 -0800

    tests: esys: audit: Fixed to not use context management
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- tests/test_esys_audit.py
+++ tests/test_esys_audit.py
@@ -2,6 +2,7 @@ from contextlib import ExitStack
 
 from tpm2_pytss.binding import *
 from tpm2_pytss.util.testing import BaseTestESYS
+from tpm2_pytss.exceptions import TPM2Error
 
 
 class TestAudit(BaseTestESYS):
@@ -60,36 +61,25 @@ class TestAudit(BaseTestESYS):
 
             creationPCR = TPML_PCR_SELECTION(count=0)
 
-            authValue_ptr = stack.enter_context(authValue.ptr())
-
-            self.esys_ctx.TR_SetAuth(ESYS_TR_RH_OWNER, authValue_ptr)
-
-            inSensitivePrimary_ptr = stack.enter_context(inSensitivePrimary.ptr())
-            inPublic_ptr = stack.enter_context(inPublic.ptr())
-            outsideInfo_ptr = stack.enter_context(outsideInfo.ptr())
-            creationPCR_ptr = stack.enter_context(creationPCR.ptr())
-            primaryHandle_node_ptr_ptr = stack.enter_context(ESYS_TR_PTR_PTR())
-            outPublic_ptr_ptr = stack.enter_context(TPM2B_PUBLIC_PTR_PTR())
-            creationData_ptr_ptr = stack.enter_context(TPM2B_CREATION_DATA_PTR_PTR())
-            creationHash_ptr_ptr = stack.enter_context(TPM2B_DIGEST_PTR_PTR())
-            creationTicket_ptr_ptr = stack.enter_context(TPMT_TK_CREATION_PTR_PTR())
+            self.esys_ctx.TR_SetAuth(ESYS_TR_RH_OWNER, authValue)
 
             signHandle = stack.enter_context(self.esys_ctx.flush_tr())
 
-            self.esys_ctx.CreatePrimary(
+            (
+                outPublic,
+                creationData,
+                creationHash,
+                creationTicket,
+            ) = self.esys_ctx.CreatePrimary(
                 ESYS_TR_RH_OWNER,
                 ESYS_TR_PASSWORD,
                 ESYS_TR_NONE,
                 ESYS_TR_NONE,
-                inSensitivePrimary_ptr,
-                inPublic_ptr,
-                outsideInfo_ptr,
-                creationPCR_ptr,
+                inSensitivePrimary,
+                inPublic,
+                outsideInfo,
+                creationPCR,
                 signHandle,
-                outPublic_ptr_ptr,
-                creationData_ptr_ptr,
-                creationHash_ptr_ptr,
-                creationTicket_ptr_ptr,
             )
 
             # /* Start a audit session */
@@ -97,8 +87,6 @@ class TestAudit(BaseTestESYS):
             sessionType = TPM2_SE_HMAC
             authHash = TPM2_ALG_SHA256
             symmetric = TPMT_SYM_DEF(algorithm=TPM2_ALG_NULL)
-
-            symmetric_ptr = stack.enter_context(symmetric.ptr())
 
             session = stack.enter_context(
                 self.esys_ctx.auth_session(
@@ -109,7 +97,7 @@ class TestAudit(BaseTestESYS):
                     ESYS_TR_NONE,
                     None,
                     sessionType,
-                    symmetric_ptr,
+                    symmetric,
                     authHash,
                 )
             )
@@ -121,11 +109,11 @@ class TestAudit(BaseTestESYS):
             prop = TPM2_PT_LOCKOUT_COUNTER
             propertyCount = 1
 
-            moreData_ptr = stack.enter_context(TPMI_YES_NO_PTR(False))
+            moreData_ptr = TPMI_YES_NO_PTR(False)
 
-            capabilityData_ptr_ptr = stack.enter_context(TPMS_CAPABILITY_DATA_PTR_PTR())
+            capabilityData_ptr = TPMS_CAPABILITY_DATA_PTR()
 
-            self.esys_ctx.GetCapability(
+            capabilityData_ptr_ptr = self.esys_ctx.GetCapability(
                 session,
                 ESYS_TR_NONE,
                 ESYS_TR_NONE,
@@ -133,49 +121,45 @@ class TestAudit(BaseTestESYS):
                 prop,
                 propertyCount,
                 moreData_ptr,
-                capabilityData_ptr_ptr,
             )
 
             privacyHandle = ESYS_TR_RH_ENDORSEMENT
             qualifyingData = TPM2B_DATA(length=0, buffer=[])
             inScheme = TPMT_SIG_SCHEME(scheme=TPM2_ALG_NULL)
 
-            qualifyingData_ptr = stack.enter_context(qualifyingData.ptr())
-            inScheme_ptr = stack.enter_context(inScheme.ptr())
-            auditInfo_ptr_ptr = stack.enter_context(TPM2B_ATTEST_PTR_PTR())
-            signature_ptr_ptr = stack.enter_context(TPMT_SIGNATURE_PTR_PTR())
-
             # Test the audit commands
-            r = self.esys_ctx.GetCommandAuditDigest(
-                privacyHandle,
-                signHandle,
-                ESYS_TR_PASSWORD,
-                ESYS_TR_PASSWORD,
-                ESYS_TR_NONE,
-                qualifyingData_ptr,
-                inScheme_ptr,
-                auditInfo_ptr_ptr,
-                signature_ptr_ptr,
-            )
-
-            if (
-                (r == TPM2_RC_COMMAND_CODE)
-                or (r == (TPM2_RC_COMMAND_CODE | TSS2_RESMGR_RC_LAYER))
-                or (r == (TPM2_RC_COMMAND_CODE | TSS2_RESMGR_TPM_RC_LAYER))
-            ):
-                self.skipTest(
-                    "Command TPM2_GetCommandAuditDigest not supported by TPM."
+            try:
+                (
+                    auditInfo_ptr_ptr,
+                    signature_ptr_ptr,
+                ) = self.esys_ctx.GetCommandAuditDigest(
+                    privacyHandle,
+                    signHandle,
+                    ESYS_TR_PASSWORD,
+                    ESYS_TR_PASSWORD,
+                    ESYS_TR_NONE,
+                    qualifyingData,
+                    inScheme,
                 )
+            except TPM2Error as error:
+                if (
+                    (error.rc == TPM2_RC_COMMAND_CODE)
+                    or (error.rc == (TPM2_RC_COMMAND_CODE | TSS2_RESMGR_RC_LAYER))
+                    or (error.rc == (TPM2_RC_COMMAND_CODE | TSS2_RESMGR_TPM_RC_LAYER))
+                ):
+                    self.skipTest(
+                        "Command TPM2_GetCommandAuditDigest not supported by TPM."
+                    )
 
-            r = self.esys_ctx.GetSessionAuditDigest(
+            self.esys_ctx.GetSessionAuditDigest(
                 privacyHandle,
                 signHandle,
                 session,
                 ESYS_TR_PASSWORD,
                 ESYS_TR_PASSWORD,
                 ESYS_TR_NONE,
-                qualifyingData_ptr,
-                inScheme_ptr,
+                qualifyingData,
+                inScheme,
                 auditInfo_ptr_ptr,
                 signature_ptr_ptr,
             )
@@ -184,19 +168,17 @@ class TestAudit(BaseTestESYS):
             clearList = TPML_CC()
             setList = TPML_CC()
 
-            setList_ptr = stack.enter_context(setList.ptr())
-            clearList_ptr = stack.enter_context(clearList.ptr())
-
-            r = self.esys_ctx.SetCommandCodeAuditStatus(
-                ESYS_TR_RH_PLATFORM,
-                ESYS_TR_PASSWORD,
-                ESYS_TR_NONE,
-                ESYS_TR_NONE,
-                auditAlg,
-                setList_ptr,
-                clearList_ptr,
-            )
-
-            if (r & ~TPM2_RC_N_MASK) == TPM2_RC_BAD_AUTH:
-                # Platform authorization not possible test will be skipped
-                self.skipTest("Platform authorization not possible.")
+            try:
+                self.esys_ctx.SetCommandCodeAuditStatus(
+                    ESYS_TR_RH_PLATFORM,
+                    ESYS_TR_PASSWORD,
+                    ESYS_TR_NONE,
+                    ESYS_TR_NONE,
+                    auditAlg,
+                    clearList,
+                    setList,
+                )
+            except TPM2Error as error:
+                if (error.rc & ~TPM2_RC_N_MASK) == TPM2_RC_BAD_AUTH:
+                    # Platform authorization not possible test will be skipped
+                    self.skipTest("Platform authorization not possible.")
