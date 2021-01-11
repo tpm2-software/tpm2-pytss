4428d5ba3cabfc2023c7054deadea6ed5dd7f45d
Daniel Gracia Pérez <50202128+dgptha@users.noreply.github.com>
2021-01-12 00:14:16 +0100

    Add test for TPML_DIGEST binding
    Signed-off-by: Daniel Gracia Pérez <50202128+dgptha@users.noreply.github.com>

--- tests/test_binding.py
+++ tests/test_binding.py
@@ -5,6 +5,7 @@ from tpm2_pytss.binding import *
 
 class TestBinding(unittest.TestCase):
     def test_set_array(self):
+        # TPML_PCR_SELECTION
         sel = TPMS_PCR_SELECTION(
             hash=TPM2_ALG_SHA256, sizeofSelect=3, pcrSelect=(0, 0, 255)
         )
@@ -12,3 +13,9 @@ class TestBinding(unittest.TestCase):
         TPMS_PCR_SELECTION(hash=TPM2_ALG_SHA256, sizeofSelect=3, pcrSelect=(0, 0, 255))
 
         sels = TPML_PCR_SELECTION(count=1, pcrSelections=(sel,))
+
+        # TPML_DIGEST
+        dig1 = TPM2B_DIGEST(size=32, buffer=bytes(range(32)))
+        dig2 = TPM2B_DIGEST(size=32, buffer=bytes(range(1, 33)))
+
+        digs = TPML_DIGEST(count=2, digests=[dig1, dig2,])
