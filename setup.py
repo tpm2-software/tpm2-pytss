c07fde227d1e24fbca38e6db9fb02d93f533fd74
William Roberts <william.c.roberts@intel.com>
2021-03-01 11:00:13 -0600

    porting CFFI PR 1248 from tpm2-tss project
    Ports https://github.com/tpm2-software/tpm2-tss/pull/1248
    to tpm2-pytss project
    
    building:
      - pip3 install --user -e .
    
    Testing:
      - python3 test/esys.py
    
    The tests pass, but a lot of work needs to be done to massage the
    interface a little more. ESYS_TR's have an esys context that could be
    removed out from underneath them, will cause flush on a dead esys
    context pointer.
    
    Initially the code had the Esys_Finalize on the __del__ attribute, but
    since control over gc essentially determines when the Esys_Context, and
    it's underlying connection to the TPM is busy, we likely want to follow
    a context manager like approach with close(), __enter__ and __exit__
    calls.
    
    Release the code under BSD-3 to match with the project.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
+import site
 import stat
+import sys
 import glob
 import shlex
 import shutil
@@ -9,6 +11,9 @@ from subprocess import check_output
 from setuptools.command.build_py import build_py
 from setuptools.command.build_ext import build_ext
 from setuptools import find_packages, setup, Extension
+
+# workaround bug https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 ORG = "tpm2-software"
 NAME = "tpm2-pytss"
@@ -21,11 +26,7 @@ IMPORT_NAME = NAME.replace("-", "_")
 
 SELF_PATH = os.path.dirname(os.path.realpath(__file__))
 
-with open(os.path.join(SELF_PATH, IMPORT_NAME, "version"), "r") as f:
-    VERSION = "".join(f).strip()
-
 README = pathlib.Path(SELF_PATH, "README.md").read_text()
-
 
 class PkgConfigNeededExtension(Extension):
     """
@@ -125,109 +126,15 @@ class PkgConfigNeededExtension(Extension):
         self._swig_opts = value
 
 
-class BuildSWIGPyFiles(build_py):
-    def run(self):
-        # This is a workaround for https://bugs.python.org/issue37247
-        # Run regular copy of files, required to run SWIG build
-        super().run()
-        # Causes SWIG python files to be built. Results in SWIG build happening
-        # twice, maybe but not sure
-        cmd = BuildExtThenCopySWIGPy(self.distribution)
-        cmd.finalize_options()
-        cmd.run()
-        # Run copy of py files again, now that SWIG has generated py files
-        super().run()
-
-
-class BuildExtThenCopySWIGPy(build_ext):
-    def run(self):
-        super().run()
-        # SWIG 4 Support
-        for fixfile in ["esys_binding.py", "fapi_binding.py"]:
-            binding_path = pathlib.Path(SELF_PATH, IMPORT_NAME, fixfile)
-            binding = binding_path.read_text()
-            if not "python_property = property" in binding:
-                binding = binding.replace("= property(", "= python_property(")
-                binding = "python_property = property\n" + binding
-                binding_path.write_text(binding)
-        # This is needed because test copies the binding files into IMPORT_NAME
-        # but build does not. Making this necessary for working with the package
-        # installed in development mode.
-        for src in glob.glob(
-            os.path.join(SELF_PATH, "build", "lib.*", "**", "*.so"), recursive=True
-        ):
-            dst = os.path.join(SELF_PATH, IMPORT_NAME, os.path.basename(src))
-            print("{} -> {}".format(src, dst))
-            shutil.copyfile(src, dst)
-            os.chmod(
-                dst,
-                stat.S_IRUSR
-                | stat.S_IWUSR
-                | stat.S_IXUSR
-                | stat.S_IRGRP
-                | stat.S_IXGRP
-                | stat.S_IROTH
-                | stat.S_IXOTH,
-            )
-
+from setuptools import setup
 
 setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=README,
-    long_description_content_type="text/markdown",
-    author=AUTHOR_NAME,
-    author_email=AUTHOR_EMAIL,
-    maintainer=AUTHOR_NAME,
-    maintainer_email=AUTHOR_EMAIL,
-    url="https://github.com/{}/{}".format(ORG, NAME),
-    license="MIT",
-    keywords=["tpm2", "security"],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-    ],
-    install_requires=INSTALL_REQUIRES,
-    tests_require=["cryptography>=2.8"],
-    extras_require={
-        "dev": [
-            "coverage",
-            "codecov",
-            "sphinx",
-            "sphinxcontrib-asyncio",
-            "black",
-            "sphinx_rtd_theme",
-        ],
-    },
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    ext_modules=[
-        PkgConfigNeededExtension(
-            "{}._esys_binding".format(IMPORT_NAME),
-            [os.path.join(IMPORT_NAME, "swig", "esys_binding.i")],
-            pkg_config_cflags=["tss2-esys", "tss2-rc", "tss2-tctildr"],
-            pkg_config_libs=["tss2-esys", "tss2-rc", "tss2-tctildr"],
-            swig_opts=["-py3", "-outdir", IMPORT_NAME],
-        ),
-        PkgConfigNeededExtension(
-            "{}._fapi_binding".format(IMPORT_NAME),
-            [os.path.join(IMPORT_NAME, "swig", "fapi_binding.i")],
-            pkg_config_cflags=["tss2-fapi", "tss2-rc", "tss2-tctildr"],
-            pkg_config_libs=["tss2-fapi", "tss2-rc", "tss2-tctildr"],
-            swig_opts=["-py3", "-outdir", IMPORT_NAME],
-        ),
-    ],
-    py_modules=[IMPORT_NAME],
-    cmdclass={"build_ext": BuildExtThenCopySWIGPy, "build_py": BuildSWIGPyFiles},
-    include_package_data=True,
-    zip_safe=False,
+    name="tpm2_pytss",
+    version="0.1",
+    description="Binding for tpm2tss (Esys and types for now",
+    url="https://github.com/tpm2-software/tpm2-tss",
+    author="Andreas Fuchs",
+    setup_requires=["cffi>=1.0.0"],
+    cffi_modules=["scripts/libesys_build.py:ffibuilder"],
+    install_requires=["cffi>=1.0.0"],
 )
