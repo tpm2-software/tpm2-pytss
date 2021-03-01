3a24af2e9e79905e6adb37e960527d92432ad8c7
William Roberts <william.c.roberts@intel.com>
2021-03-01 11:00:13 -0600

    porting CFFI PR 1248 from tpm2-tss project
    Ports https://github.com/tpm2-software/tpm2-tss/pull/1248
    to tpm2-pytss project
    
    building:
      - pip3 install --user -e .
    
    Testing:
      - python3 test/esys.py
    
    The tests pass, but a lot of work needs to be done to massage the
    interface a little more. ESYS_TR's have an esys context that could be
    removed out from underneath them, will cause flush on a dead esys
    context pointer.
    
    Initially the code had the Esys_Finalize on the __del__ attribute, but
    since control over gc essentially determines when the Esys_Context, and
    it's underlying connection to the TPM is busy, we likely want to follow
    a context manager like approach with close(), __enter__ and __exit__
    calls.
    
    Release the code under BSD-3 to match with the project.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

---
+++ scripts/libesys_build.py
@@ -0,0 +1,51 @@
+from cffi import FFI
+ffibuilder = FFI()
+
+import os
+import sys
+
+# Set up the search path so we find prepare_header and other modules
+PATH = os.path.dirname(__file__) if len(os.path.dirname(__file__)) > 0 else os.getcwd()
+if not os.path.isabs(PATH):
+    PATH = os.path.join(os.getcwd(), PATH)
+
+print("adding path: {}".format(PATH))
+sys.path.insert(0, PATH)
+from prepare_headers import prepare
+
+# XXX For now just hardcode this, but we probably want to look at how to reuse the
+# pkgconfig extension.
+#
+# Generate a massaged header file for cffi bindings
+#
+
+tss2_header_dirs = [
+    '/usr/include',
+    '/usr/local/include',
+]
+
+found_dir = None
+for hd in tss2_header_dirs:
+    full_path = os.path.join(hd, 'tss2')
+    if os.path.isdir(full_path):
+        found_dir = hd
+        break
+if found_dir is None:
+    sys.exit('Could not find esys headers in {}'.format(tss2_header_dirs))
+
+# strip tss2 prefix
+prepare(found_dir, 'libesys.h')
+
+ffibuilder.cdef(open('libesys.h').read())
+
+# so it is often just the "#include".
+ffibuilder.set_source('tpm2_pytss._libesys',
+"""
+     /* the C header of the library */
+     #include <tss2/tss2_esys.h>
+""",
+     debug=True,
+     libraries=['tss2-esys'])   # library name, for the linker
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
---
+++ test/esys.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3 -u
+"""
+SPDX-License-Identifier: BSD-3
+"""
+
+import os
+import sys
+import subprocess
+import unittest
+from time import sleep
+
+from tpm2_pytss import *
+
+class Tpm(object):
+    def __init__(self):
+        print("Setting up simulator: ", end='')
+        self.tpm = subprocess.Popen(['tpm_server', '-rm'])
+        sleep(2)
+        if self.tpm.poll() is not None:
+            print("tpm_server not started, retrying...")
+            sleep(5)
+            self.tpm = subprocess.Popen(['tpm_server'])
+            sleep(2)
+        if self.tpm.poll() is not None:
+            print("tpm_server not started, SKIPPING")
+            exit(77) #Skipped
+        print("OK")
+
+    def close(self):
+        self.tpm.terminate()
+        os.unlink("NVChip")
+
+class TestPyEsys(unittest.TestCase):
+    tpm = None
+
+    @classmethod
+    def setUpClass(cls):
+        # This assumes that mssim or something similar is started and needs a startup command
+        TestPyEsys.tpm = Tpm()
+        with EsysContext() as ectx:
+            ectx.Startup(TPM2_SU.CLEAR)
+
+    def setUp(self):
+        self.ectx = EsysContext()
+
+    def tearDown(self):
+        self.ectx.close()
+        self.ectx = None
+
+    @classmethod
+    def tearDownClass(cls):
+        TestPyEsys.tpm.close()
+
+    def testGetRandom(self):
+        sys.stderr.write("PYTEST BILL GETRANDOM: {}\n".format(self.ectx))
+        r = self.ectx.GetRandom(5)
+        self.assertTrue(len(r) == 5)
+
+    def testCreatePrimary(self):
+        sys.stderr.write("PYTEST BILL CREATEPRIMARY: {}".format(self.ectx))
+        inSensitive = TPM2B_SENSITIVE_CREATE()
+        inPublic = TPM2B_PUBLIC()
+        outsideInfo = TPM2B_DATA()
+        creationPCR = TPML_PCR_SELECTION()
+
+        inPublic.publicArea.type = TPM2_ALG.ECC
+        inPublic.publicArea.nameAlg = TPM2_ALG.SHA1
+        inPublic.publicArea.objectAttributes = TPMA_OBJECT.USERWITHAUTH | \
+                                       TPMA_OBJECT.SIGN_ENCRYPT | \
+                                       TPMA_OBJECT.RESTRICTED | \
+                                       TPMA_OBJECT.FIXEDTPM | \
+                                       TPMA_OBJECT.FIXEDPARENT | \
+                                       TPMA_OBJECT.SENSITIVEDATAORIGIN
+
+        inPublic.publicArea.parameters.eccDetail.scheme.scheme = TPM2_ALG.ECDSA
+        inPublic.publicArea.parameters.eccDetail.scheme.details.ecdsa.hashAlg = TPM2_ALG.SHA256
+        inPublic.publicArea.parameters.eccDetail.symmetric.algorithm = TPM2_ALG.NULL
+        inPublic.publicArea.parameters.eccDetail.kdf.scheme = TPM2_ALG.NULL
+        inPublic.publicArea.parameters.eccDetail.curveID = TPM2_ECC.NIST_P256
+
+        self.ectx.tr.OWNER.setAuth("")
+
+        x, _, _, _, _ = self.ectx.CreatePrimary(self.ectx.tr.OWNER,
+                        inSensitive, inPublic, outsideInfo, creationPCR,
+                        session1=self.ectx.tr.PASSWORD)
+        print(x)
+
+if __name__ == "__main__":
+    unittest.main()
