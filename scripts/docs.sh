6950ff4809d46e9910d96eed98f35bbad5120bd5
John Andersen <johnandersenpdx@gmail.com>
2021-03-02 22:02:18 -0800

    scripts/docs.sh: Enable Sphinx equivalent of -Werror
    Add -W flag to Sphinx docs build to ensure that docs builds fail if any
    warnings are present. Warnings in Sphinx are almost always worth failing
    the CI for, so we want to keep -W on the build command line.
    
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf pages
-sphinx-build -b html docs pages
+sphinx-build -W -b html docs pages
 find pages/ -name \*.html -exec \
   sed -i 's/<span class="gp">\&gt;\&gt;\&gt; <\/span>//g' {} \;
 touch pages/.nojekyll
