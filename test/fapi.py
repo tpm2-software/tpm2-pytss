5f08b12737819157ed1e66d21d5bd0184ffb8b90
Johannes Holland <johannes.holland@infineon.com>
2021-03-06 14:13:39 +0100

    test: add first fapi test as proof of concept
    Signed-off-by: Johannes Holland <johannes.holland@infineon.com>

---
+++ test/fapi.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3 -u
+"""
+SPDX-License-Identifier: BSD-3
+"""
+
+import unittest
+
+from tpm2_pytss import *
+from TSS2_BaseTest import TSS2_FapiTest
+
+
+class TestFapi(TSS2_FapiTest):
+    def testProvision(self):
+        r = self.fapi.provision()
+        self.assertEqual(r, False)
+
+    def testGetRandom(self):
+        r = self.fapi.get_random(42)
+        self.assertEqual(len(r), 42)
+
+
+if __name__ == "__main__":
+    unittest.main()
