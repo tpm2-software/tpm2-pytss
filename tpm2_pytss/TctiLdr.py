c9f9810ec6d637ad01ac9b371f27dd3e86184320
Erik Larsson <who+github@cnackers.org>
2021-03-08 22:32:14 +0100

    tctildr: make the TCTI context a read only property
    Signed-off-by: Erik Larsson <who+github@cnackers.org>

--- tpm2_pytss/TctiLdr.py
+++ tpm2_pytss/TctiLdr.py
@@ -10,7 +10,7 @@ from .utils import _chkrc
 class TctiLdr:
     def __init__(self, name=None, conf=None):
 
-        self.ctx_pp = ffi.new("TSS2_TCTI_CONTEXT **")
+        self._ctx_pp = ffi.new("TSS2_TCTI_CONTEXT **")
 
         if name is None:
             name = ffi.NULL
@@ -28,8 +28,8 @@ class TctiLdr:
         if not isinstance(conf, (bytes, type(ffi.NULL))):
             raise RuntimeError(f"name must be of type bytes, got {type(name)}")
 
-        _chkrc(lib.Tss2_TctiLdr_Initialize_Ex(name, conf, self.ctx_pp))
-        self.ctx = self.ctx_pp[0]
+        _chkrc(lib.Tss2_TctiLdr_Initialize_Ex(name, conf, self._ctx_pp))
+        self._ctx = self._ctx_pp[0]
 
     def __enter__(self):
         return self
@@ -38,5 +38,9 @@ class TctiLdr:
         self.close()
 
     def close(self):
-        lib.Tss2_TctiLdr_Finalize(self.ctx_pp)
-        self.ctx = ffi.NULL
+        lib.Tss2_TctiLdr_Finalize(self._ctx_pp)
+        self._ctx = ffi.NULL
+
+    @property
+    def ctx(self):
+        return self._ctx
