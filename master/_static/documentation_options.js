12e6c0c657f12c8f3a0a59645feb0b653f7ac9e8
John Andersen <johnandersenpdx@gmail.com>
2021-03-11 18:33:29 +0000

    docs: Thu Mar 11 18:33:29 UTC 2021
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gce18532',
+    VERSION: '0.1.dev1+g08864d8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
