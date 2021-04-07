1705458ac36afccb58b79cb5034a2acd69338f20
William Roberts <william.c.roberts@intel.com>
2021-04-07 11:25:25 -0500

    ESAPI: modify and test NV basic commands
    Test that:
     - NV_Define
     - NV_Write
     - NV_Read
     - NV_Undefine
    
    For basic NV configurations work. Also, modify the arguments to NV_Read
    and NV_Write so we can just default the authHandle to the nvIndex handle
    if the authHandle is not specified. This makes calls simpler in the more
    common auth to the handle role.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/test_esapi.py
+++ test/test_esapi.py
@@ -61,6 +61,45 @@ class TestEsys(TSS2_EsapiTest):
         for d in digests[1:]:
             self.assertEqual(len(d), 32)
 
+    def test_plain_NV_define_write_read_undefine(self):
+
+        nv_public = TPM2B_NV_PUBLIC(
+            nvPublic=TPMS_NV_PUBLIC(
+                nvIndex=TPM2_HC.NV_INDEX_FIRST,
+                nameAlg=TPM2_ALG.SHA256,
+                attributes=TPMA_NV.parse("ownerread|ownerwrite|authread|authwrite"),
+                dataSize=32,
+            )
+        )
+
+        # No password NV index
+        nv_index = self.ectx.NV_DefineSpace(ESYS_TR.OWNER, None, nv_public)
+        self.ectx.NV_Write(nv_index, b"hello world")
+
+        value = self.ectx.NV_Read(nv_index, 11)
+        self.assertEqual(value, b"hello world")
+
+        public, name = self.ectx.NV_ReadPublic(nv_index)
+        self.assertEqual(public.nvPublic.nvIndex, TPM2_HC.NV_INDEX_FIRST)
+        self.assertEqual(public.nvPublic.nameAlg, TPM2_ALG.SHA256)
+        self.assertEqual(
+            public.nvPublic.attributes,
+            TPMA_NV.parse("ownerread|ownerwrite|authread|authwrite|written"),
+        )
+        self.assertEqual(public.nvPublic.authPolicy.size, 0)
+        self.assertEqual(public.nvPublic.dataSize, 32)
+        # Algorithm id (UINT16) followed by SHA256 len of name bytes
+        self.assertEqual(len(name), 2 + 32)
+
+        n = str(name)
+        self.assertEqual(len(n), 68)
+        self.assertTrue(isinstance(n, str))
+
+        self.ectx.NV_UndefineSpace(ESYS_TR.OWNER, nv_index)
+
+        with self.assertRaises(TSS2_Exception):
+            public, name = self.ectx.NV_ReadPublic(nv_index)
+
 
 if __name__ == "__main__":
     unittest.main()
