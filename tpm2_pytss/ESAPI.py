327bd67600608250543a15b77a17d53451212553
Erik Larsson <who+github@cnackers.org>
2021-06-07 02:57:14 +0200

    test_esapi.py: add DictionaryAttackLockReset test
    Signed-off-by: Erik Larsson <who+github@cnackers.org>

--- tpm2_pytss/ESAPI.py
+++ tpm2_pytss/ESAPI.py
@@ -2385,12 +2385,16 @@ class ESAPI:
 
     def DictionaryAttackLockReset(
         self,
-        lockHandle,
-        session1=ESYS_TR.NONE,
+        lockHandle=ESYS_TR.RH_LOCKOUT,
+        session1=ESYS_TR.PASSWORD,
         session2=ESYS_TR.NONE,
         session3=ESYS_TR.NONE,
     ):
 
+        check_handle_type(lockHandle, "lockHandle")
+        check_handle_type(session1, "session1")
+        check_handle_type(session2, "session2")
+        check_handle_type(session3, "session3")
         _chkrc(
             lib.Esys_DictionaryAttackLockReset(
                 self.ctx, lockHandle, session1, session2, session3
