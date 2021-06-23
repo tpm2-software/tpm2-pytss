87e379e615132e2c2c4aeb928042cf27aa5f9543
Erik Larsson <who+github@cnackers.org>
2021-06-23 20:57:07 +0200

    add test for ClearControl
    Signed-off-by: Erik Larsson <who+github@cnackers.org>

--- tpm2_pytss/ESAPI.py
+++ tpm2_pytss/ESAPI.py
@@ -2569,10 +2569,18 @@ class ESAPI:
         self,
         auth,
         disable,
-        session1=ESYS_TR.NONE,
+        session1=ESYS_TR.PASSWORD,
         session2=ESYS_TR.NONE,
         session3=ESYS_TR.NONE,
     ):
+
+        check_handle_type(
+            auth, "auth", expected=(ESYS_TR.RH_PLATFORM, ESYS_TR.RH_LOCKOUT)
+        )
+
+        check_handle_type(session1, "session1")
+        check_handle_type(session2, "session2")
+        check_handle_type(session3, "session3")
 
         _chkrc(
             lib.Esys_ClearControl(self.ctx, auth, session1, session2, session3, disable)
