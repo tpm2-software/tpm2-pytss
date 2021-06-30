ac711cbb54f737bcde138a60fb002e2d9dd4d311
William Roberts <william.c.roberts@intel.com>
2021-06-30 12:22:57 -0500

    test: Esys_PolicyAuthorizeNV
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tpm2_pytss/ESAPI.py
+++ tpm2_pytss/ESAPI.py
@@ -2563,13 +2563,24 @@ class ESAPI:
 
     def PolicyAuthorizeNV(
         self,
-        authHandle,
         nvIndex,
         policySession,
-        session1=ESYS_TR.NONE,
+        authHandle=0,
+        session1=ESYS_TR.PASSWORD,
         session2=ESYS_TR.NONE,
         session3=ESYS_TR.NONE,
     ):
+
+        if authHandle == 0:
+            authHandle = nvIndex
+
+        check_handle_type(authHandle, "authHandle")
+        check_handle_type(nvIndex, "nvIndex")
+        check_handle_type(policySession, "policySession")
+
+        check_handle_type(session1, "session1")
+        check_handle_type(session2, "session2")
+        check_handle_type(session3, "session3")
 
         _chkrc(
             lib.Esys_PolicyAuthorizeNV(
