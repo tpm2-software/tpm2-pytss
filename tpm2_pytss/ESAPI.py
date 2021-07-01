675ed52ca9b4b974c6e4bfc86c781c54569fa918
William Roberts <william.c.roberts@intel.com>
2021-07-01 12:51:17 -0500

    ESAPI: PEP8 Commit
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tpm2_pytss/ESAPI.py
+++ tpm2_pytss/ESAPI.py
@@ -1614,10 +1614,10 @@ class ESAPI:
         )
         return (TPM2B_ATTEST(get_ptr(timeInfo)), TPMT_SIGNATURE(get_ptr(signature)))
 
-    def Commit(
+    def commit(
         self,
-        signHandle,
-        P1,
+        sign_handle,
+        p1,
         s2,
         y2,
         session1=ESYS_TR.PASSWORD,
@@ -1625,12 +1625,12 @@ class ESAPI:
         session3=ESYS_TR.NONE,
     ):
 
-        check_handle_type(signHandle, "signHandle")
+        check_handle_type(sign_handle, "sign_handle")
         check_handle_type(session1, "session1")
         check_handle_type(session2, "session2")
         check_handle_type(session3, "session3")
 
-        P1_cdata = get_cdata(P1, TPM2B_ECC_POINT, "P1")
+        P1_cdata = get_cdata(p1, TPM2B_ECC_POINT, "p1")
         s2_cdata = get_cdata(s2, TPM2B_SENSITIVE_DATA, "s2")
         y2_cdata = get_cdata(y2, TPM2B_ECC_PARAMETER, "y2")
 
@@ -1641,7 +1641,7 @@ class ESAPI:
         _chkrc(
             lib.Esys_Commit(
                 self.ctx,
-                signHandle,
+                sign_handle,
                 session1,
                 session2,
                 session3,
