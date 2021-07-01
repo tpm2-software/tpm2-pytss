9e7d914fb023a068d544b11bbca703e547f02afa
William Roberts <william.c.roberts@intel.com>
2021-07-01 12:46:54 -0500

    ESAPI: PEP8 EventSequenceComplete
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/test_esapi.py
+++ test/test_esapi.py
@@ -1619,35 +1619,35 @@ class TestEsys(TSS2_EsapiTest):
 
         self.ectx.sequence_update(seqHandle, None)
 
-        pcrs = self.ectx.EventSequenceComplete(
+        pcrs = self.ectx.event_sequence_complete(
             ESYS_TR.PCR16, seqHandle, "AnotherBuffer"
         )
         self.assertEqual(type(pcrs), TPML_DIGEST_VALUES)
 
         with self.assertRaises(TypeError):
-            self.ectx.EventSequenceComplete(object(), seqHandle, None)
+            self.ectx.event_sequence_complete(object(), seqHandle, None)
 
         with self.assertRaises(ValueError):
-            self.ectx.EventSequenceComplete(42, seqHandle, None)
+            self.ectx.event_sequence_complete(42, seqHandle, None)
 
         with self.assertRaises(TypeError):
-            self.ectx.EventSequenceComplete(ESYS_TR.PCR16, 46.5, None)
+            self.ectx.event_sequence_complete(ESYS_TR.PCR16, 46.5, None)
 
         with self.assertRaises(TypeError):
-            self.ectx.EventSequenceComplete(ESYS_TR.PCR16, seqHandle, object())
+            self.ectx.event_sequence_complete(ESYS_TR.PCR16, seqHandle, object())
 
         with self.assertRaises(TypeError):
-            self.ectx.EventSequenceComplete(
+            self.ectx.event_sequence_complete(
                 ESYS_TR.PCR16, seqHandle, None, sequence1=67.34
             )
 
         with self.assertRaises(TypeError):
-            self.ectx.EventSequenceComplete(
+            self.ectx.event_sequence_complete(
                 ESYS_TR.PCR16, seqHandle, None, sequence2="boo"
             )
 
         with self.assertRaises(TypeError):
-            self.ectx.EventSequenceComplete(
+            self.ectx.event_sequence_complete(
                 ESYS_TR.PCR16, seqHandle, None, sequence3=object()
             )
 
