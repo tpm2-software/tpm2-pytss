b241dd5a3939d1d01c58873eb139c6d2e5e4736b
William Roberts <william.c.roberts@intel.com>
2021-07-01 12:53:07 -0500

    ESAPI: PEP8 SetCommandCodeAuditStatus
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/test_esapi.py
+++ test/test_esapi.py
@@ -3384,42 +3384,46 @@ class TestEsys(TSS2_EsapiTest):
 
     def test_SetCommandCodeAuditStatus(self):
 
-        self.ectx.SetCommandCodeAuditStatus(TPM2_ALG.SHA256, TPML_CC(), TPML_CC())
+        self.ectx.set_command_code_audit_status(TPM2_ALG.SHA256, TPML_CC(), TPML_CC())
 
         with self.assertRaises(TypeError):
-            self.ectx.SetCommandCodeAuditStatus(42.6, TPML_CC(), TPML_CC())
+            self.ectx.set_command_code_audit_status(42.6, TPML_CC(), TPML_CC())
 
         with self.assertRaises(ValueError):
-            self.ectx.SetCommandCodeAuditStatus(42, TPML_CC(), TPML_CC())
+            self.ectx.set_command_code_audit_status(42, TPML_CC(), TPML_CC())
 
         with self.assertRaises(TypeError):
-            self.ectx.SetCommandCodeAuditStatus(TPM2_ALG.SHA256, TPML_ALG(), TPML_CC())
+            self.ectx.set_command_code_audit_status(
+                TPM2_ALG.SHA256, TPML_ALG(), TPML_CC()
+            )
 
         with self.assertRaises(TypeError):
-            self.ectx.SetCommandCodeAuditStatus(TPM2_ALG.SHA256, TPML_CC(), object())
+            self.ectx.set_command_code_audit_status(
+                TPM2_ALG.SHA256, TPML_CC(), object()
+            )
 
         with self.assertRaises(TypeError):
-            self.ectx.SetCommandCodeAuditStatus(
+            self.ectx.set_command_code_audit_status(
                 TPM2_ALG.SHA256, TPML_CC(), TPML_CC(), auth=45.6
             )
 
         with self.assertRaises(ValueError):
-            self.ectx.SetCommandCodeAuditStatus(
+            self.ectx.set_command_code_audit_status(
                 TPM2_ALG.SHA256, TPML_CC(), TPML_CC(), auth=ESYS_TR.ENDORSEMENT
             )
 
         with self.assertRaises(TypeError):
-            self.ectx.SetCommandCodeAuditStatus(
+            self.ectx.set_command_code_audit_status(
                 TPM2_ALG.SHA256, TPML_CC(), TPML_CC(), session1=45.6
             )
 
         with self.assertRaises(TypeError):
-            self.ectx.SetCommandCodeAuditStatus(
+            self.ectx.set_command_code_audit_status(
                 TPM2_ALG.SHA256, TPML_CC(), TPML_CC(), session2="baz"
             )
 
         with self.assertRaises(TypeError):
-            self.ectx.SetCommandCodeAuditStatus(
+            self.ectx.set_command_code_audit_status(
                 TPM2_ALG.SHA256, TPML_CC(), TPML_CC(), session3=[]
             )
 
