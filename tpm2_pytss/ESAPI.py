5c509f05296a3fcb5cf9363ce03998145286fa68
William Roberts <william.c.roberts@intel.com>
2021-07-06 13:26:39 -0500

    ESAPI: PEP8 ChangeEPS
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tpm2_pytss/ESAPI.py
+++ tpm2_pytss/ESAPI.py
@@ -2751,21 +2751,21 @@ class ESAPI:
 
         _chkrc(lib.Esys_ChangePPS(self.ctx, auth_handle, session1, session2, session3))
 
-    def ChangeEPS(
+    def change_eps(
         self,
-        authHandle=ESYS_TR.RH_PLATFORM,
+        auth_handle=ESYS_TR.RH_PLATFORM,
         session1=ESYS_TR.PASSWORD,
         session2=ESYS_TR.NONE,
         session3=ESYS_TR.NONE,
     ):
 
-        check_handle_type(authHandle, "authHandle", expected=(ESYS_TR.RH_PLATFORM,))
+        check_handle_type(auth_handle, "auth_handle", expected=(ESYS_TR.RH_PLATFORM,))
 
         check_handle_type(session1, "session1")
         check_handle_type(session2, "session2")
         check_handle_type(session3, "session3")
 
-        _chkrc(lib.Esys_ChangeEPS(self.ctx, authHandle, session1, session2, session3))
+        _chkrc(lib.Esys_ChangeEPS(self.ctx, auth_handle, session1, session2, session3))
 
     def Clear(
         self,
