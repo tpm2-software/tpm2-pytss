6e634cc31104bea3fa1c648bbb741358995c4523
William Roberts <william.c.roberts@intel.com>
2021-09-29 14:10:56 -0500

    test: add main to test_exception.py
    So this works (from project root):
    python3 -m test.test_exception
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- test/test_exception.py
+++ test/test_exception.py
@@ -38,3 +38,7 @@ class ExceptionTest(unittest.TestCase):
         self.assertEqual(exc.handle, 0)
         self.assertEqual(exc.parameter, 0)
         self.assertEqual(exc.session, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
