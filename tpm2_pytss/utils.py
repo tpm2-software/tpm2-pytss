63b4427ad851caf587ec5ed05d334a2a066788b1
William Roberts <william.c.roberts@intel.com>
2021-11-18 16:00:38 -0600

    utils: add unwrap
    Add the inverse operation of wrap, unwrap, which essentially allows for
    TPM key exportation given a key with proper attributes. By duplicating
    the key to a parent where one controls or has access to the private key,
    the returned TPM2B_PRIVATE from the TPM2_Duplicate command can be
    unwrapped into the TPMT_SENSITIVE. From there, folks can furlough that
    key if needed, or use it in their favorite crypto lib.
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- tpm2_pytss/utils.py
+++ tpm2_pytss/utils.py
@@ -2,11 +2,15 @@ from .internal.crypto import (
     _kdfa,
     _get_digest,
     _symdef_to_crypt,
+    _secret_to_seed,
     _generate_seed,
+    _decrypt,
     _encrypt,
+    _check_hmac,
     _hmac,
 )
 from .types import *
+from cryptography.hazmat.primitives import constant_time as ct
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 from typing import Optional, Tuple
@@ -127,3 +131,99 @@ def wrap(
     duplicate = TPM2B_PRIVATE(buffer=hmacdata + dupsens)
 
     return (enckeyout, duplicate, outsymseed)
+
+
+def unwrap(
+    newparentpub: TPMT_PUBLIC,
+    newparentpriv: TPMT_SENSITIVE,
+    public: TPM2B_PUBLIC,
+    duplicate: TPM2B_PRIVATE,
+    outsymseed: TPM2B_ENCRYPTED_SECRET,
+    symkey: Optional[bytes] = None,
+    symdef: Optional[TPMT_SYM_DEF_OBJECT] = None,
+) -> TPM2B_SENSITIVE:
+    """unwraps a key under a TPM key hierarchy. In essence, export key from TPM.
+
+    This is the inverse function to the wrap() routine. This is usually performed by the TPM when importing
+    objects, however, if an object is duplicated under a new parent where one has both the public and private
+    keys, the object can be unwrapped.
+
+    Args:
+        newparentpub (TPMT_PUBLIC): The public area of the parent the key was duplicated/wrapped under.
+        newparentpriv(TPMT_SENSITIVE): The private key of the parent the key was duplicated/wrapped under.
+        public (TPM2B_PUBLIC): The public area of the key to be unwrapped.
+        duplicate(TPM2B_PRIVATE): The private or wrapped key to be unwrapped.
+        outsymseed(TPM2B_ENCRYPTED_SECRET): The output symmetric seed from a wrap or duplicate call.
+        symkey (bytes or None): Symmetric key for inner encryption. Defaults to None. When None
+        and symdef is defined a key will be generated based on the key size for symdef.
+        symdef (TPMT_SYMDEF_OBJECT): Symmetric algorithm to be used for inner encryption, defaults to None.
+        If None no inner wrapping is performed, else this should be set to aes128CFB since that is what
+        the TPM supports. To set to aes128cfb, do:
+        TPMT_SYM_DEF(
+          algorithm=TPM2_ALG.AES,
+          keyBits=TPMU_SYM_KEY_BITS(sym=128),
+          mode=TPMU_SYM_MODE(sym=TPM2_ALG.CFB),
+        )
+
+    Returns:
+        A tuple of (TPM2B_DATA, TPM2B_PRIVATE, TPM2B_ENCRYPTED_SECRET) which is the encryption key, the
+        the wrapped duplicate and the encrypted seed.
+
+    Raises:
+        ValueError: If the public key type or symmetric algorithm are not supported
+    """
+    halg = _get_digest(newparentpub.nameAlg)
+
+    seed = _secret_to_seed(newparentpriv, newparentpub, b"DUPLICATE\x00", outsymseed)
+    hmackey = _kdfa(
+        newparentpub.nameAlg, seed, b"INTEGRITY", b"", b"", halg.digest_size * 8
+    )
+
+    buffer = bytes(duplicate)
+
+    hmacdata, offset = TPM2B_DIGEST.unmarshal(buffer)
+    outerhmac = bytes(hmacdata)
+
+    dupsens = buffer[offset:]
+    name = bytes(public.get_name())
+    _check_hmac(halg, hmackey, dupsens, name, outerhmac)
+
+    cipher, _, bits = _symdef_to_crypt(newparentpub.parameters.asymDetail.symmetric)
+    outerkey = _kdfa(newparentpub.nameAlg, seed, b"STORAGE", name, b"", bits)
+
+    sensb = _decrypt(cipher, outerkey, dupsens)
+
+    if symdef and symdef.algorithm != TPM2_ALG.NULL:
+        if not symkey:
+            raise RuntimeError(
+                "Expected symkey when symdef is not None or Tsymdef.algorithm is not TPM2_ALG.NULL"
+            )
+
+        cipher, mode, bits = _symdef_to_crypt(symdef)
+        halg = _get_digest(public.publicArea.nameAlg)
+
+        # unwrap the inner encryption which is the integrity + TPM2B_SENSITIVE
+        innerint_and_decsens = _decrypt(cipher, symkey, sensb)
+        innerint, offset = TPM2B_DIGEST.unmarshal(innerint_and_decsens)
+        innerint = bytes(innerint)
+        decsensb = innerint_and_decsens[offset:]
+
+        h = hashes.Hash(halg(), backend=default_backend())
+        h.update(decsensb)
+        h.update(name)
+        integrity = h.finalize()
+
+        if not ct.bytes_eq(integrity, innerint):
+            raise RuntimeError("Expected inner integrity to match")
+
+        decsens = decsensb
+    else:
+        decsens = sensb
+
+    s, l = TPM2B_SENSITIVE.unmarshal(decsens)
+    if len(decsens) != l:
+        raise RuntimeError(
+            f"Expected the sensitive buffer to be size {l}, got: {len(encdupsens)}"
+        )
+
+    return s
