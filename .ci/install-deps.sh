491660489251ea8b273d07cbceeb5dbf3bb2e5f5
Erik Larsson <who+github@cnackers.org>
2022-02-21 19:44:29 +0100

    ci: add tpm2-tools to CI dependencies
    Signed-off-by: Erik Larsson <who+github@cnackers.org>

--- .ci/install-deps.sh
+++ .ci/install-deps.sh
@@ -5,6 +5,7 @@ set -exo pipefail
 
 export TPM2_TSS_VERSION=${TPM2_TSS_VERSION:-"3.0.3"}
 export TPM2_TSS_FAPI=${TPM2_TSS_FAPI:-"true"}
+export TPM2_TOOLS_VERSION=${TPM2_TOOLS_VERSION:-"5.2"}
 
 #
 # Get dependencies for building and install tpm2-tss and abrmd projects
@@ -105,6 +106,25 @@ else
   popd
   rm -fr /tmp/ibmswtpm2
 fi
+
+#
+# Install tpm2-tools
+#
+if ! command -v tpm2; then
+  # for git describe to work, one needs either a tag or a deep clone of master.
+  if [ "${TPM2_TOOLS_VERSION}" != "master" ]; then
+    extra_git_flags="--depth 1"
+  fi
+  git -C /tmp clone ${extra_git_flags} \
+    --branch "${TPM2_TOOLS_VERSION}" https://github.com/tpm2-software/tpm2-tools.git
+  pushd /tmp/tpm2-tools
+  ./bootstrap
+  ./configure CFLAGS=-g --disable-fapi
+  make -j$(nproc)
+  sudo make install
+  popd
+fi
+
 
 #
 # Pip version 21.3 was broken with in-pace (-e) installs. Thus use something
