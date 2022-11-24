0fa41c540369852cf47893012d2808e71d8e0e4c
William Roberts <william.c.roberts@intel.com>
2022-11-24 09:32:52 -0600

    internal/utils: add class for version strings
    Add a class specifically designed to deal with git version strings. The
    previous munging to pep-440 didn't work in all cases correctly. This
    class treats git describe strings as demonstrated by the example below
    in order of lowest to highes:
    
      - 2.0.1-rc0
      - 2.0.1-rc0-dirty
      - 2.0.1-rc0-2-g0fd1c5fbbaf2
      - 2.0.1-rc0-2-g0fd1c5fbbaf2-dirty
      - 2.0.1
      - 2.0.1-5-g0fd1c5fbbaf2
      - 2.0.1-5-g0fd1c5fbbaf2-dirty
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- src/tpm2_pytss/internal/utils.py
+++ src/tpm2_pytss/internal/utils.py
@@ -2,7 +2,6 @@
 import logging
 import sys
 from typing import List
-from packaging.version import Version, InvalidVersion
 
 from .._libtpm2_pytss import ffi, lib
 from ..TSS2_Exception import TSS2_Exception
@@ -20,6 +19,151 @@ logger = logging.getLogger(__name__)
 
 # Peek into the loaded modules, if mock is loaded, set __MOCK__ to True, else False
 __MOCK__ = "unittest.mock" in sys.modules
+
+
+class TSS2Version:
+    """ Class for comparing git describe output
+
+    Motivation:
+    python's packaging version class follows pep-440, however
+    that system was found incapable of reasoning about git
+    describe output even when munging version strings into
+    pep-440, I could not find a semantic that provided the
+    correct ordering.
+
+    This class takes a git describe string allows one to
+    compare them to eachother like Pythons packaging.Version
+    class.
+
+    Args:
+        version(str): git describe --always --dirty output.
+    """
+
+    def __init__(self, version: str):
+        self._version = version
+        major = "0"
+        minor = "0"
+        patch = "0"
+
+        rc = "0xFFFFFFFF"
+        commits = "0"
+
+        is_rc = "rc" in version
+        is_dirty = "dirty" in version
+        hunks = version.split(".")
+        extra_data = version.split("-")[1:]
+
+        def handle_extra():
+            nonlocal extra_data
+            nonlocal commits
+            nonlocal rc
+            nonlocal version
+            nonlocal is_rc
+            # rc0-26-g1234-dirty
+            if len(extra_data) == 4:
+                if not is_rc:
+                    raise ValueError(f'Invalid version string, got: "{version}"')
+
+                rc = extra_data[0][2:]
+                commits = extra_data[1]
+            # 26-g1234-dirty OR rc5-26-g1234 OR 1
+            elif len(extra_data) == 3:
+                if is_rc:
+                    rc = extra_data[0][2:]
+                    commits = extra_data[1]
+                elif is_dirty:
+                    commits = extra_data[0]
+                else:
+                    raise ValueError(f'Invalid version string, got: "{version}"')
+            # 26-g1234 OR rc0-dirty
+            elif len(extra_data) == 2:
+                if is_rc:
+                    rc = extra_data[0][2:]
+                else:
+                    commits = extra_data[0]
+            # rc0 OR dirty
+            elif len(extra_data) == 1:
+                if is_rc:
+                    rc = extra_data[0][2:]
+                elif not is_dirty:
+                    commits = extra_data[0]
+            elif len(extra_data) == 0:
+                # No extra data to process, thats OK
+                pass
+            else:
+                raise ValueError(f'Invalid version string, got: "{version}"')
+
+        def cleanse(xstr):
+            if "-" in xstr:
+                return xstr[: xstr.find("-")]
+
+            return xstr
+
+        # 4, 4-76, 4-rc5, 4-rc5-26, 4-26-g1234-dirty, 4-rc0-26-g1234-dirty
+        if len(hunks) == 1:
+            major = cleanse(hunks[0])
+            minor = "0"
+            patch = "0"
+            handle_extra()
+
+        # 4.0, 4.0-g1234-76, 4.0-rc5, 4.0-rc5-26-g1234, 4.0-26-g1234-dirty, 4.0-rc0-26-g1234-dirty
+        elif len(hunks) == 2:
+            major = hunks[0]
+            minor = cleanse(hunks[1])
+            patch = "0"
+            handle_extra()
+
+        # 4.0, 4.0-76-g1234, 4.0-rc5, 4.0-rc5-26-g1234, 4.0-26-g1234-dirty, 4.0-rc0-26-g1234-dirty
+        elif len(hunks) == 3:
+            major = hunks[0]
+            minor = hunks[1]
+            patch = cleanse(hunks[2])
+            handle_extra()
+
+        else:
+            raise ValueError(f'Invalid version string, got: "{version}"')
+
+        # Convert to int
+        major = int(major, 0).to_bytes(4, byteorder="big")
+        minor = int(minor, 0).to_bytes(4, byteorder="big")
+        patch = int(patch, 0).to_bytes(4, byteorder="big")
+        rc = int(rc, 0).to_bytes(4, byteorder="big")
+        commits = int(commits, 0).to_bytes(4, byteorder="big")
+        dirty = int(is_dirty).to_bytes(1, byteorder="big")
+
+        # TO make reasoning easy we lay out a big int where each field
+        # can hold 4 bytes of data, except for dirty which is a byte
+        # MAJOR : MINOR : PATCH : RC : COMMITS : DIRTY
+        concatenated = major + minor + patch + rc + commits + dirty
+        v = int.from_bytes(concatenated, byteorder="big")
+        self._value = v
+
+    def __str__(self):
+        return self._version
+
+    def __lt__(self, other):
+        x = other if isinstance(other, int) else other._value
+        return self._value < x
+
+    def __lte__(self, other):
+        x = other if isinstance(other, int) else other._value
+        return self._value <= x
+
+    def __eq__(self, other):
+        x = other if isinstance(other, int) else other._value
+        return self._value == x
+
+    def __ne__(self, other):
+        x = other if isinstance(other, int) else other._value
+        return self._value != x
+
+    def __ge__(self, other):
+        x = other if isinstance(other, int) else other._value
+        return self._value >= x
+
+    def __gt__(self, other):
+        x = other if isinstance(other, int) else other._value
+        return self._value > x
 
 
 def _chkrc(rc, acceptable=None):
@@ -241,48 +385,11 @@ def _check_bug_fixed(
         logger.warning(message)
 
 
-def _lib_version_normalize(version: str) -> Version:
-    """ Normalize a git describe version string to a PEP 440 version string
-
-    Normalize git describe --always --dirty version strings for Python's packing
-    Version class to be happy. That library takes PEP440 strings as defined in:
-      - https://peps.python.org/pep-0440/
-
-    | case | inputs | output         |
-    | ---- | -------| -------------- |
-    | 0    | 3.1.0-126-g0fd1c5fbbaf2      | X.Y.devM = 3.1.0.dev126       |
-    | 1    | 1.1.0-13-gb574eae194a2-dirty | X.YaN.devM = 3.1.0a126.dev126 |
-    | 2    | 1.1.0-rc0                    | X.yrcN = 1.1.0rc0             |
-    | 3    | 1.1.0                        | X.Y = 1.1.0                   |
-    """
-
-    chunks = version.split("-")
-    normalized = None
-    # case 0
-    if len(chunks) == 3:
-        normalized = f"{chunks[0]}.dev{chunks[1]}"
-    # case 1
-    elif len(chunks) == 4:
-        normalized = f"{chunks[0]}a{chunks[1]}.dev{chunks[1]}"
-    # case 2
-    elif len(chunks) == 2:
-        normalized = f"{chunks[0]}{chunks[1]}"
-    # case 3
-    elif len(chunks) == 1:
-        normalized = f"{chunks[0]}"
-    else:
-        raise InvalidVersion(
-            f"Expected at most 4 dash delimited version information, got: {version}"
-        )
-
-    return Version(normalized)
-
-
 def _lib_version_atleast(tss2_lib, version):
     if tss2_lib not in _versions:
         return False
 
-    libv = _lib_version_normalize(_versions[tss2_lib])
-    lv = _lib_version_normalize(version)
+    libv = TSS2Version(_versions[tss2_lib])
+    lv = TSS2Version(version)
 
     return libv >= lv
